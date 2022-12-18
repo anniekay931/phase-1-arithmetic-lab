373a1fe69329af561aa6a67a8f9d6bace3ac9da5
Annika Kriskovich <annika.krisko2013@gmail.com>
2022-12-18 10:46:58 -0600

    commit tree 58ca87e1e7fab8792941937ff87f0fe4eab1eba7

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 10;
+let nums = [1, 5, 20, 10];
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let mod = num3 % num4;
+let max = Math.max(...nums);
